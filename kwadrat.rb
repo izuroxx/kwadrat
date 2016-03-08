afade82978f345ba99e2b1650e792be83e41655a
Izuroxx <izu.i.cnr@gmail.com>
2016-03-08 08:32:33 +0100

    initial commit

---
+++ kwadrat.rb
@@ -0,0 +1,8 @@
+# encoding: utf-8
+puts ""
+puts "Obliczanie powierzchni kwadratu"
+puts ""
+print "Podaj długość boku kwadratu: "
+	a=gets
+	pk=a.to_f**2
+	puts ("Powierzchnia kwadratu wynosi: " + pk.to_s)
